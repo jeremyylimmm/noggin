9976948473a17336364abe584dd2fc8878824ce5
jeremyylimmm <jeremylim.0304@gmail.com>
2026-06-10 02:57:32 +1000

    Fix rook value bench: 1590705

--- src/lib.rs
+++ src/lib.rs
@@ -154,7 +154,7 @@ impl Piece {
             Piece::Pawn => 100,
             Piece::Knight => 300,
             Piece::Bishop => 300,
-            Piece::Rook => 5000,
+            Piece::Rook => 500,
             Piece::Queen => 900,
             Piece::King => 0,
         }
