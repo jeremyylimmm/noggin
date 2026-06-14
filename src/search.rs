366838d1de04a7121ecc8a87dcce94c1b49625f3
jeremyylimmm <jeremylim.0304@gmail.com>
2026-06-15 00:49:54 +1000

    Fix bug again bench: 734454

--- src/search.rs
+++ src/search.rs
@@ -406,7 +406,7 @@ impl Searcher {
 
         let pv_node = beta > alpha + 1;
 
-        if pos.is_threefold_repetition() {
+        if pos.is_threefold_repetition() || (!in_check && pos.halfmove_clock >= 100) {
             return 0;
         }
 
@@ -493,10 +493,6 @@ impl Searcher {
             return -MATE_SCORE + ply as i32;
         }
 
-        if pos.halfmove_clock >= 100 {
-            return 0;
-        }
-
         self.tt_set(
             pos.hash,
             best_move,
@@ -540,7 +536,7 @@ impl Searcher {
         let side = pos.to_move;
         let in_check = pos.checked(side);
 
-        if pos.is_threefold_repetition() || pos.halfmove_clock >= 100 {
+        if pos.is_threefold_repetition() || (!in_check && pos.halfmove_clock >= 100) {
             return (0, NULL_MOVE);
         }
 
